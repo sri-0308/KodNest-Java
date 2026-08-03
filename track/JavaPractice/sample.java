af6186832e42dd778b38629dbd1d0aec27bac113
sri-0308 <s.srilakshmi@gmail.com>
2026-08-03 16:06:51 +0530

    practice session 1

---
+++ track/JavaPractice/sample.java
@@ -0,0 +1,15 @@
+
+public class Sample {
+
+    public static void main(String[] args) {
+// Write your code here.
+        String learnerName = "Ananya";
+        int completedProblems = 25;
+        double practiceHours = 2.5;
+        boolean projectCompleted = true;
+        System.out.println("Learner: " + learnerName);
+        System.out.println("Problems Completed: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectCompleted);
+    }
+}
