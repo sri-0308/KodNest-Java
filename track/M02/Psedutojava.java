63efe70916abda5962eeff5e5de9857076b8b5e6
sri-0308 <s.srilakshmi@gmail.com>
2026-08-03 18:47:07 +0530

    pseudotojava

---
+++ track/M02/Psedutojava.java
@@ -0,0 +1,16 @@
+
+public class Pseudotojava {
+
+    public static void main(String[] args) {
+// Convert the given pseudocode into Java code
+        int jhours = 2;
+        int ahours = 1;
+        int Prepdays = 5;
+        int weekj = jhours * Prepdays;
+        int weeka = ahours * Prepdays;
+        int total = weekj + weeka;
+        System.out.println("Java: " + weekj);
+        System.out.println("Aptitude: " + weeka);
+        System.out.println("Total: " + total);
+    }
+}
