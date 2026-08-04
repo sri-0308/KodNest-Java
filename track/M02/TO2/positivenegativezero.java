8c88416aee7edb0ac0c5e7fb164ecffaddb9bc84
sri-0308 <s.srilakshmi@gmail.com>
2026-08-04 12:29:02 +0530

    postivenegativezero

---
+++ track/M02/TO2/positivenegativezero.java
@@ -0,0 +1,14 @@
+
+class positivenegativezero {
+
+    public static void main(String args[]) {
+        int num = 25;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
