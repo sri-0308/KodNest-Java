db2529ae40edad95cdfa702b6670859d05e9e47c
sri-0308 <s.srilakshmi@gmail.com>
2026-08-06 12:51:14 +0530

    breakandcontinue using nested

---
+++ track/M02/TO2/continuecondition.java
@@ -0,0 +1,12 @@
+
+class continuecondition {
+
+    public static void main(String args[]) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
